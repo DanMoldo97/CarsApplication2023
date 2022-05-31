03ed84851e01e67574bdb022f149609ce1d59d0c
Daniel Moldoveanu <daniel.moldoveanu@abslbs.com>
2022-05-31 11:29:09 +0300

    added new car photo capture activity

--- app/src/main/java/com/example/carsapplication/MainActivity.kt
+++ app/src/main/java/com/example/carsapplication/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_home, R.id.nav_manufacturers), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_home, R.id.nav_manufacturers, R.id.nav_car_capture), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
---
+++ app/src/main/java/com/example/carsapplication/ui/camera_capture/CarCapture.kt
@@ -0,0 +1,16 @@
+package com.example.carsapplication.ui.camera_capture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.carsapplication.R
+
+class CarCapture : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_car_capture)
+    }
+
+
+
+}
