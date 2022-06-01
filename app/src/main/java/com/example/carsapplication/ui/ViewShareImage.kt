d49b1571875ec47a45e235d2b7c8cf7cadbebfc5
Daniel Moldoveanu <daniel.moldoveanu@abslbs.com>
2022-06-01 11:49:42 +0300

    addded menu for share

--- app/src/main/java/com/example/carsapplication/ui/ViewShareImage.kt
+++ app/src/main/java/com/example/carsapplication/ui/ViewShareImage.kt
@@ -8,14 +8,43 @@ import com.example.carsapplication.R
 import android.provider.MediaStore
 
 import android.graphics.Bitmap
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
+import android.widget.SearchView
+import android.widget.Toast
+import com.example.carsapplication.databinding.ActivityCarCaptureBinding
+import com.example.carsapplication.databinding.ActivityViewShareImageBinding
 
 class ViewShareImage : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityViewShareImageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_share_image)
+        viewBinding = ActivityViewShareImageBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
         val uri: Uri? = intent.data
         val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
-        var imgView : ImageView = findViewById(R.id.imageViewShare)
-        imgView.setImageBitmap(bitmap)
+        viewBinding.imageViewShare.setImageBitmap(bitmap)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        var inflater = menuInflater
+        inflater.inflate(R.menu.share_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share -> shareImg()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun shareImg() {
+        Toast.makeText(this, "Am ajuns aici!", Toast.LENGTH_SHORT).show()
     }
 }
